181d64f565a86948f29a5b9ab0a3ec10a5dea4fe
Ilia Akatov <ilia-akatoff@ya.ru>
2024-09-16 22:02:35 +0300

    Created all views and urls for lessons and courses.

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
