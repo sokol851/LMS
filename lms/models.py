cbf36acc51e1e11609724b47b99996ac2125c69e
Ilia Akatov <ilia-akatoff@ya.ru>
2024-09-16 21:33:34 +0300

    Added all models

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    preview = models.ImageField(upload_to='course/', default='course/non_course.png', verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(upload_to='lesson/', default='lesson/non_lesson.png', verbose_name='Превью', **NULLABLE)
+    url_video = models.TextField(verbose_name='Ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='курс')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    first_name = models.CharField(max_length=150, default='Не указано', verbose_name='Имя', **NULLABLE)
+    last_name = models.CharField(max_length=150, default='Не указано', verbose_name='Фамилия', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE, default='Не указано')
+    city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE, default='Не указано')
+    avatar = models.ImageField(upload_to='users/%Y', default='users/non_avatar.png', verbose_name='Аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
