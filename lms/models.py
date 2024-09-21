5fce60a69d07be1ee22f9dbfb4cdab5caa5ef13f
Ilia Akatov <ilia-akatoff@ya.ru>
2024-09-21 14:01:38 +0300

    Edited model for images.

--- lms/models.py
+++ lms/models.py
@@ -7,7 +7,8 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Course(models.Model):
     name = models.CharField(max_length=150, verbose_name='Название')
-    preview = models.ImageField(upload_to='course/', default='course/non_course.png', verbose_name='Превью', **NULLABLE)
+    preview = models.ImageField(upload_to='static/course/covers/', default='course/non_course.png',
+                                verbose_name='ПревьюКурса', **NULLABLE)
     description = models.TextField(verbose_name='Описание')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
 
@@ -22,7 +23,8 @@ class Course(models.Model):
 class Lesson(models.Model):
     name = models.CharField(max_length=150, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
-    preview = models.ImageField(upload_to='lesson/', default='lesson/non_lesson.png', verbose_name='Превью', **NULLABLE)
+    preview = models.ImageField(upload_to='static/lesson/covers/', default='lesson/non_lesson.png',
+                                verbose_name='ПревьюУрока', **NULLABLE)
     url_video = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='курс')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец', **NULLABLE)
