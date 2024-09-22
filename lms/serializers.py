6c7929b231a3b9ebc9cb4cd1742c23dadd6012b8
Ilia Akatov <ilia-akatoff@ya.ru>
2024-09-22 16:54:42 +0300

    Edited requirements.txt Added in serializer Course 2 fields (lessons and count) Added serializer in Payment Added Django-filter Added new ViewList for Payment

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,15 +3,21 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
-    lesson_count = serializers.SerializerMethodField()
-
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ('name', 'preview', 'description', 'lesson_count', 'lessons',)
+
+    @staticmethod
+    def get_lesson_count(instance):
+        lesson_count = instance.lessons.count()
+        return lesson_count
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+from users.models import Payment
+from users.serializers import PaymentSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_courses', 'paid_lesson', 'paid_type',)
+    ordering_fields = ('data_payment',)
